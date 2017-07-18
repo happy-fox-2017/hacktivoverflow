ee92151663394ce491682d34be7e49573d97e746
myyusuf <myyusuf1911@gmail.com>
2017-07-18 16:17:57 +0700

    Add port configuration

--- server/app.js
+++ server/app.js
@@ -13,7 +13,24 @@ app.use(bodyParser.json());
 app.use('/api/users', userRouter);
 app.use('/api/questions', questionRouter);
 
-app.listen(3000, function () {
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+const port = normalizePort(process.env.PORT || '3000');
+
+app.listen(port, function () {
   console.log('App listening on port 3000!')
 });
 
